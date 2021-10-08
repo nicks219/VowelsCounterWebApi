c9b5082530302bd0df82eebc0f55eec967625f56
Nikolay Medvedev <nick219@yandex.ru>
2021-10-09 01:32:08 +0300

    Add project files.

---
+++ TMG1DotNetCoreWPF/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TMG1DotNetCoreWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class TheMostGame : Window
+    {
+        private readonly Parser _parser;
+
+        public TheMostGame()
+        {
+            InitializeComponent();
+            Button c = LayoutRoot.Children.OfType<Button>().First(i => i.Name == "send");
+            c.Click += Button_Click;
+            _parser = new();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            textBlock.Text = string.Empty;
+            words.Text = string.Empty;
+            vowels.Text = string.Empty;
+            _parser.GrubIdFrom(textBox);
+
+            foreach (string json in _parser.GetDataFromServer())
+            {
+                string text = _parser.ParseJson(json);
+                textBlock.Text += text;
+                textBlock.Text += "\n\n";
+                words.Text += _parser.CountWords(text).ToString() + "\n\n";
+                vowels.Text += _parser.CountVowels(text).ToString() + "\n\n";
+            }
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+        }
+
+        private void TextBox_TextPreview(object sender, TextCompositionEventArgs e)
+        {
+            _parser.CreateIdList(e, textBox);
+        }
+    }
+}
---
+++ TMG1DotNetCoreWPF/Parser.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace TMG1DotNetCoreWPF
+{
+    internal class Parser
+    {
+        private HashSet<int> _identificators;
+        private readonly List<char> _charsUnicodeSpecials;
+        private readonly string _regexUnicodeSpecials;
+        private readonly string token = "";
+
+        internal Parser()
+        {
+            _charsUnicodeSpecials = new List<char> { '\u04e7', '\u0456' };
+            //Unicode: x00c0-x00ff (192-255)
+            for (int i = 192; i <= 255; i++)
+            {
+                _charsUnicodeSpecials.Add((char)i);
+            }
+            _regexUnicodeSpecials = new string(_charsUnicodeSpecials.ToArray());
+        }
+
+        internal List<string> GetDataFromServer()
+        {
+            List<string> result = new();
+
+            try
+            {
+                using WebClient wc = new WebClient { Proxy = null };
+                wc.Headers.Add("TMG-Api-Key", token);
+                foreach (int id in _identificators)
+                {
+                    result.Add(wc.DownloadString("https://tmgwebtest.azurewebsites.net/api/textstrings/" + id.ToString()));
+                }
+            }
+            catch (WebException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            return result;
+        }
+
+        internal void CreateIdList(TextCompositionEventArgs e, TextBox textBox)
+        {
+            e.Handled = " ,;0123456789".IndexOf(e.Text) == -1;
+            //Correction after input
+            if (",;".IndexOf(e.Text) != -1)
+            {
+                GrubIdFrom(textBox);
+                BuildTextFromIds();
+                e.Handled = true;
+            }
+
+            void BuildTextFromIds()
+            {
+                StringBuilder result = new();
+                foreach (var r in _identificators)
+                {
+                    result.Append(r);
+                    result.Append(", ");
+                }
+                textBox.Text = result.ToString();
+                textBox.Focus();
+                textBox.SelectionStart = textBox.Text.Length;
+            }
+        }
+
+        internal void GrubIdFrom(TextBox textBox)
+        {
+            string[] text = textBox.Text.Split(',', ';');
+            _identificators = new();
+            foreach (string identificator in text)
+            {
+                bool success = Int32.TryParse(identificator, out int id);
+                if (success)
+                {
+                    if (id is >= 1 and <= 20)
+                    {
+                        //Added only correct Ids
+                        _identificators.Add(id);
+                    }
+                }
+
+            }
+        }
+
+        internal string ParseJson(String input)
+        {
+            if (!Regex.IsMatch(input, "^({\"text\":\").*(\"})$"))
+            {
+                MessageBox.Show("Sorry, server goes bad..");
+                return string.Empty;
+            }
+            var x = Regex.Replace(input, "^({\"text\":\")|(\"})$", "");
+            StringBuilder result = new StringBuilder(x);
+            return result.ToString();
+        }
+
+        internal int CountVowels(String input)
+        {
+            StringBuilder result = new StringBuilder();
+            //I can count only chars, but not "vowels" - because it's the sound.
+            return new Regex("[aeiouAEIOUяиюыаоэуеёЯИЮЫАОЭУЕЁ" + _regexUnicodeSpecials + "]").Matches(input).Count;
+        }
+
+        internal int CountWords(String input)
+        {
+            return input.Split(' ').Count();
+        }
+
+        private void GetHeaders(WebClient wc)
+        {
+            foreach (string n in wc.ResponseHeaders.Keys)
+            {
+                //textBlock.Text += n + " = " + wc.ResponseHeaders[n] + "\n";}
+            }
+        }
+    }
+
+}
