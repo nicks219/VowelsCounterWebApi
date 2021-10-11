b1a8f79aae629aa7ae3e914cdd19e3da68b6bd33
Nikolay Medvedev <nick219@yandex.ru>
2021-10-11 23:42:21 +0300

    Оптимизация: regex'ы создаются один раз на тип.

--- TMG1DotNetCoreWPF/Parser.cs
+++ TMG1DotNetCoreWPF/Parser.cs
@@ -7,27 +7,15 @@ namespace TMG1DotNetCoreWPF
     // Accepts Unicode accented characters
     internal class Parser
     {
-        private readonly List<char> _charsUnicodeDiacritical;
-        private readonly string _unicodeVowelsPattern;
         //Unicode Diacritical Range: x00c0-x00ff
         private const int ACCENTED_RANGE_MIN = 192;
         private const int ACCENTED_RANGE_MAX = 255;
         //Standart vowels:
         private const string REGULAR_VOWELS = "aeiouAEIOUяиюыаоэуеёЯИЮЫАОЭУЕЁ";
-
-        /// <summary>
-        /// Creates a regex pattern to parse text with accented characters
-        /// </summary>
-        internal Parser()
-        {
-            //Single Unicode accented characters:
-            _charsUnicodeDiacritical = new List<char> { '\u04e7', '\u0456' };
-            for (int i = ACCENTED_RANGE_MIN; i <= ACCENTED_RANGE_MAX; i++)
-            {
-                _charsUnicodeDiacritical.Add((char)i);
-            }
-            _unicodeVowelsPattern = "[" + REGULAR_VOWELS + new string(_charsUnicodeDiacritical.ToArray()) + "]";
-        }
+        //RegEx patterns:
+        private static readonly Regex _jsonTestRegex = new("^({\"text\":\").*(\"})$");
+        private static readonly Regex _jsonReplaceRegex = new("^({\"text\":\")|(\"})$");
+        private static readonly Regex _unicodeVowelsPattern = InitVowelsRegex();
 
         /// <summary>
         /// Trims json brackets
@@ -36,12 +24,12 @@ namespace TMG1DotNetCoreWPF
         /// <returns>String without json brackets</returns>
         internal string TrimJson(string input)
         {
-            if (!Regex.IsMatch(input, _jsonTestRegex))
+            if (!_jsonTestRegex.IsMatch(input))
             {
                 MessageBox.Show("Sorry, server goes bad..");
                 return string.Empty;
             }
-            return Regex.Replace(input, _jsonReplaceRegex, "");
+            return _jsonReplaceRegex.Replace(input, "");
         }
 
         /// <summary>
@@ -52,7 +40,7 @@ namespace TMG1DotNetCoreWPF
         internal int CountVowels(string input)
         {
             //I can only count symbols, not "vowels" - because it is sound.
-            return new Regex(_unicodeVowelsPattern).Matches(input).Count;
+            return _unicodeVowelsPattern.Matches(input).Count;
         }
 
         /// <summary>
@@ -65,8 +53,19 @@ namespace TMG1DotNetCoreWPF
             return input.Split(' ').Length;
         }
 
-        //RegEx patterns:
-        private static readonly string _jsonTestRegex = "^({\"text\":\").*(\"})$";
-        private static readonly string _jsonReplaceRegex = "^({\"text\":\")|(\"})$";
+        /// <summary>
+        /// Creates a regex pattern to parse text with accented characters
+        /// </summary>
+        private static Regex InitVowelsRegex()
+        {
+            List<char> _charsUnicodeDiacritical;
+            //Single Unicode accented characters:
+            _charsUnicodeDiacritical = new List<char> { '\u04e7', '\u0456' };
+            for (int i = ACCENTED_RANGE_MIN; i <= ACCENTED_RANGE_MAX; i++)
+            {
+                _charsUnicodeDiacritical.Add((char)i);
+            }
+            return new Regex("[" + REGULAR_VOWELS + new string(_charsUnicodeDiacritical.ToArray()) + "]");
+        }
     }
 }
